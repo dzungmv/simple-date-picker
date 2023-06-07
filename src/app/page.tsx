a2bea6982f58c3cd8f66c8cb2523496243a4ff04
Dzung <jungjung@Dzungs-MacBook-Pro.local>
2023-06-07 15:03:52 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,178 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+import { useEffect, useRef, useState } from 'react';
+
+interface DatePickerProps {
+    callback: (date: string) => void;
+}
+
+export default function DatePicker({ callback }: DatePickerProps) {
+    const [day, setDay] = useState<string>('');
+    const [month, setMonth] = useState<string>('');
+    const [year, setYear] = useState<string>('');
+
+    const dayBoxRef = useRef<HTMLDivElement>(null);
+    const monthBoxRef = useRef<HTMLDivElement>(null);
+    const yearBoxRef = useRef<HTMLDivElement>(null);
+
+    const [dayBox, setDayBox] = useState<boolean>(false);
+    const [monthBox, setMonthBox] = useState<boolean>(false);
+    const [yearBox, setYearBox] = useState<boolean>(false);
+
+    const days = Array.from(Array(31), (_, i) => i + 1);
+    const months = Array.from(Array(12), (_, i) => i + 1);
+    const startYear = 1929;
+    const endYear = new Date().getFullYear();
+    const years = Array.from(
+        { length: endYear - startYear },
+        (_, i) => startYear + i + 1
+    );
+
+    useEffect(() => {
+        if (day && month && year) {
+            const date = `${day}/${month}/${year}`;
+            callback(date);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [day, month, year]);
+
+    useEffect(() => {
+        const clickOutSide = (e: any) => {
+            if (dayBoxRef.current && !dayBoxRef.current.contains(e.target)) {
+                setDayBox(false);
+            }
+        };
+
+        document.addEventListener('mousedown', clickOutSide);
+
+        return () => {
+            document.removeEventListener('mousedown', clickOutSide);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [dayBoxRef.current]);
+
+    useEffect(() => {
+        const clickOutSide = (e: any) => {
+            if (
+                monthBoxRef.current &&
+                !monthBoxRef.current.contains(e.target)
+            ) {
+                setMonthBox(false);
+            }
+        };
+
+        document.addEventListener('mousedown', clickOutSide);
+
+        return () => {
+            document.removeEventListener('mousedown', clickOutSide);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [monthBoxRef.current]);
+
+    useEffect(() => {
+        const clickOutSide = (e: any) => {
+            if (yearBoxRef.current && !yearBoxRef.current.contains(e.target)) {
+                setYearBox(false);
+            }
+        };
+
+        document.addEventListener('mousedown', clickOutSide);
+
+        return () => {
+            document.removeEventListener('mousedown', clickOutSide);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [yearBoxRef.current]);
+    return (
+        <div className='w-full flex items-center gap-2'>
+            <div className='w-full relative'>
+                <span
+                    className='pl-2 py-2 border rounded-lg flex items-center justify-between hover:bg-gray-200 cursor-pointer select-none'
+                    onClick={() => setDayBox((prev) => !prev)}
+                >
+                    {day ? day : 'Day'}
+                </span>
+                {dayBox && (
+                    <div
+                        ref={dayBoxRef}
+                        className=' absolute top-[100%] bg-white shadow-sm w-full rounded-md mt-1 border py-1 transition-colors h-[300px] overflow-auto'
+                    >
+                        {days.map((item: number, index: number) => {
+                            return (
+                                <div
+                                    className='w-full hover:bg-gray-300 p-1 px-2 py-2 cursor-pointer text-sm'
+                                    onClick={() => {
+                                        setDay(item.toString());
+                                        setDayBox(false);
+                                    }}
+                                    key={index}
+                                >
+                                    {item}
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
+            <div className='w-full relative'>
+                <span
+                    className='pl-2 py-2 border rounded-lg flex items-center justify-between hover:bg-gray-200 cursor-pointer select-none'
+                    onClick={() => setMonthBox((prev) => !prev)}
+                >
+                    {month ? month : 'Month'}
+                </span>
+                {monthBox && (
+                    <div
+                        ref={monthBoxRef}
+                        className=' absolute top-[100%] bg-white shadow-sm w-full rounded-md mt-1 border py-1 transition-colors h-[300px] overflow-auto'
+                    >
+                        {months.map((item: number, index: number) => {
+                            return (
+                                <div
+                                    className='w-full hover:bg-gray-300 p-1 px-2 py-2 cursor-pointer text-sm'
+                                    onClick={() => {
+                                        setMonth(item.toString());
+                                        setMonthBox(false);
+                                    }}
+                                    key={index}
+                                >
+                                    {item}
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
+
+            <div className='w-full relative'>
+                <span
+                    className='pl-2 py-2 border rounded-lg flex items-center justify-between hover:bg-gray-200 cursor-pointer select-none'
+                    onClick={() => setYearBox((prev) => !prev)}
+                >
+                    {year ? year : 'Year'}
+                </span>
+                {yearBox && (
+                    <div
+                        ref={yearBoxRef}
+                        className=' absolute top-[100%] bg-white shadow-sm w-full rounded-md mt-1 border py-1 transition-colors h-[300px] overflow-auto'
+                    >
+                        {years.map((item: number, index: number) => {
+                            return (
+                                <div
+                                    className='w-full hover:bg-gray-300 p-1 px-2 py-2 cursor-pointer text-sm'
+                                    onClick={() => {
+                                        setYear(item.toString());
+                                        setYearBox(false);
+                                    }}
+                                    key={index}
+                                >
+                                    {item}
+                                </div>
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
